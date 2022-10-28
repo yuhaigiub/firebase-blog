a9e8c7d9a71e34ac26ec7a2efd085d392c248afc
Yuhaigiub <buigiahuynt@gmail.com>
2022-10-28 22:08:27 +0700

    change database to ytest-project

--- src/firebase-config.tsx
+++ src/firebase-config.tsx
@@ -3,9 +3,9 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_API_KEY,
-	authDomain: "yuhaigiub.firebaseapp.com",
-	projectId: "yuhaigiub",
-	storageBucket: "yuhaigiub.appspot.com",
+	authDomain: "ytest-project.firebaseapp.com",
+	projectId: "ytest-project",
+	storageBucket: "ytest-project.appspot.com",
 	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 	appId: process.env.REACT_APP_APP_ID,
 	measurementId: process.env.REACT_APP_MEASUREMENT_ID,
